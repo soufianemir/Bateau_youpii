088bfee41bb4830af641464248b9aaf7a7d763e8
soufianemir <soufiane.mir@gmail.com>
2026-08-08 20:09:37 +0200

    Fix offline PWA asset cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'marine-companion-v1';
-const APP_SHELL = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './assets/icon-192.png', './assets/icon-512.png', './assets/apple-touch-icon.png'];
+const CACHE = 'marine-companion-v2';
+const APP_SHELL = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './assets/icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
